a221dafce3b53fe7a24ba7b38b345b954dcbfdc5
Ann Leleko <vredina.ann@gmail.com>
2019-04-13 21:43:14 +0300

    done 9 section from homepage without styles

--- template_parts/homepage/section_9.php
+++ template_parts/homepage/section_9.php
@@ -3,8 +3,6 @@
     <?php $title = get_sub_field('title'); ?>
     <?php $text = get_sub_field('description'); ?>
     <?php $image = get_sub_field('image'); ?>
-    <?php $link_1 = get_sub_field('link_1'); ?>
-    <?php $link_2 = get_sub_field('link_2'); ?>
 
     <div>
       <div class="picture">
@@ -25,19 +23,6 @@
         </div>
       <?php endif; ?>
 
-      <div class="link">
-        <?php if (!empty($link_1)): ?>
-        <a href="<?php echo $link_1['url']; ?>" class="btn">
-          <?php endif; ?>
-        </a>
-
-        <?php if (!empty($link_2)): ?>
-        <a href="<?php echo $link_2['url']; ?>" class="btn">
-          <?php endif; ?>
-        </a>
-      </div>
-
     </div>
   </div>
 </section>
-
